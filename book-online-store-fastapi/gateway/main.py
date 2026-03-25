6ab22d7d4ab706e649ed30d288d1ef6eaf9f2527
Dilshani Senavirathna <141391107+Dilshani16@users.noreply.github.com>
2026-03-25 17:39:22 +0530

    fixed gateway

--- book-online-store-fastapi/gateway/main.py
+++ book-online-store-fastapi/gateway/main.py
@@ -5,6 +5,24 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, ConfigDict, Field
+from typing import List
+
+# class Item(BaseModel):
+#     model_config = ConfigDict(populate_by_name=True)
+    
+#     book_id: str = Field(validation_alias="bookId")
+#     title: str
+#     quantity: int
+#     price: float
+
+# class OrderCreate(BaseModel):
+#     model_config = ConfigDict(populate_by_name=True)
+    
+#     customer_id: str = Field(validation_alias="customerId")
+#     items: List[Item]
+#     status: str = "Pending"
+#     address: str
 
 app = FastAPI(title="API Gateway", version="1.0.0")
 
@@ -168,27 +186,44 @@ async def get_all_orders():
 
 
 @app.get("/gateway/orders/{order_id}")
-async def get_order(order_id: int):
+async def get_order(order_id: str):
     return await forward_request("orders", f"/api/orders/{order_id}", "GET")
 
 
 @app.get("/gateway/orders/customer/{customer_id}")
-async def get_customer_orders(customer_id: int):
+async def get_customer_orders(customer_id: str):
     return await forward_request("orders", f"/api/orders/customer/{customer_id}", "GET")
-
 
 @app.post("/gateway/orders")
 async def create_order(request: Request):
     body = await request.json()
-    return await forward_request("orders", "/api/orders", "POST", json_body=body)
-
+    return await forward_request( "orders", "/api/orders" "POST", json_body=body)
+    
+# @app.post("/gateway/orders")
+# async def create_order(order: OrderCreate):
+#     return await forward_request(
+#         "orders",
+#         "/api/orders",
+#         "POST",
+#         json_body=order.dict(by_alias=True)
+#     )
 
 @app.put("/gateway/orders/{order_id}")
-async def update_order(order_id: int, request: Request):
+async def update_order(order_id: str, request: Request):
     body = await request.json()
-    return await forward_request("orders", f"/api/orders/{order_id}", "PUT", json_body=body)
+    return await forward_request( "orders", f"/api/orders/{order_id}", "PUT",json_body=body)
+
+    
+# @app.put("/gateway/orders/{order_id}")
+# async def update_order(order_id: str, order: OrderCreate):
+#     return await forward_request(
+#         "orders",
+#         f"/api/orders/{order_id}",
+#         "PUT",
+#         json_body=order.dict(by_alias=True)
+#     )
 
 
 @app.delete("/gateway/orders/{order_id}")
-async def delete_order(order_id: int):
+async def delete_order(order_id: str):
     return await forward_request("orders", f"/api/orders/{order_id}", "DELETE")
