fed4feb2bfc59c3cb04933580f8e319bd627fe6e
A H M Prabhath Dilshan Abeysinghe <prabhath@AHM-Prabhath-Dilshan-Abeysinghes-MacBook-Air.local>
2026-03-30 21:25:41 +0530

    Configure MongoDB settings for book and customer services

---
+++ book-online-store-fastapi/book-service/config.py
@@ -0,0 +1,8 @@
+import os
+
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://ramyamarappullige21_db_user:12345@cluster0.805wiu9.mongodb.net/bookstore?retryWrites=true&w=majority&ssl=true",
+)
+DB_NAME = os.getenv("DB_NAME", "bookstore")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "books")
